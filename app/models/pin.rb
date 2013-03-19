d2f78d7355f46b5457a67954c7bf50f4f3789774
Gary Adams <gary@techhealthedu.com>
2013-03-18 21:32:06 -0400

    Testing image upload

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,9 +1,13 @@
 class Pin < ActiveRecord::Base
+attr_accessible :description, :image
 
-  attr_accessible :description
 
   validates :description, presence: true
-
-  belongs_to :user
   validates :user_id, presence: true
+  validates_attachment :image, presence: true,
+  								content_type: { content_type: ['image/jpeg' , 'image/jpg' , 'image/png' , 'image/gif'] }
+  
+  belongs_to :user
+  has_attached_file :image, styles: { medium: "320x240>"}
+  
 end
