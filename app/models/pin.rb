721ca4bc48290e416009b2f5ce2d815ec81e0ef2
Gary Adams <gary@techhealthedu.com>
2013-03-19 14:41:10 -0400

    Pins with image

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -8,6 +8,6 @@ attr_accessible :description, :image
   								content_type: { content_type: ['image/jpeg' , 'image/jpg' , 'image/png' , 'image/gif'] }
   
   belongs_to :user
-  has_attached_file :image, styles: { medium: "320x240>"}
+  has_attached_file :image, styles: { medium: "400x400>"}
   
 end
